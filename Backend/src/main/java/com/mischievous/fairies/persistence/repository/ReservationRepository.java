fa61e49dddd11c4a7b742ae15ee0c3fd3977c9bf
Alexander Dyankov <alexander.dyankov@gmail.com>
2026-03-28 03:30:15 +0200

    Added get methods for reservations

--- Backend/src/main/java/com/mischievous/fairies/persistence/repository/ReservationRepository.java
+++ Backend/src/main/java/com/mischievous/fairies/persistence/repository/ReservationRepository.java
@@ -1,6 +1,9 @@
 package com.mischievous.fairies.persistence.repository;
 
+import com.mischievous.fairies.persistence.model.FoodSaleEntity;
 import com.mischievous.fairies.persistence.model.ReservationEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +14,11 @@ public interface ReservationRepository extends JpaRepository<ReservationEntity, 
     Optional<ReservationEntity> findByPaymentIntentId(String paymentIntentId);
 
     void deleteByPaymentIntentId(String paymentIntentId);
+
+    Page<ReservationEntity> findByAccount_Id(Long accountId, Pageable pageable);
+
+    Page<ReservationEntity> findByFoodSaleEntity_Id(Long foodSaleEntityId, Pageable pageable);
+
+    Page<ReservationEntity> findByFoodSaleEntity_Food_Restaurant_Id(Long restaurantId, Pageable pageable);
+
 }
